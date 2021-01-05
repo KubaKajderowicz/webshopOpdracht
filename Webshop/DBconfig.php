bc771e799c5f9288983885da440eac14974f4fc9
KubaKajderowicz <54847310+KubaKajderowicz@users.noreply.github.com>
2021-01-05 14:43:52 +0100

    Initial Commit

---
+++ Webshop/DBconfig.php
@@ -0,0 +1,16 @@
+<?php
+
+DEFINE("USER", "root");
+DEFINE("PASSWORD","");
+
+try {
+    $verbinding = new 
+    PDO("mysql:host=localhost;dbname=webshopPHP",USER,PASSWORD);
+    $verbinding->setAttribute
+    (PDO::ATTR_ERRMODE,PDO::ERRMODE_EXCEPTION);
+}catch(PDOException $e) {
+    echo $e->getMessage();
+    echo "Kon geen verbinding maken.";
+}
+
+?>
